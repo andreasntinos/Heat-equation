94fe3fd4388547cdf3107dca94cd1cc385c58b22
andreasntinos <adreasdinos10@gmail.com>
2025-06-18 22:40:32 +0200

    Update laser_test.py with validation plot

--- laser_test.py
+++ laser_test.py
@@ -4,6 +4,7 @@ import numpy as np
 import ufl
 from petsc4py import PETSc
 import os
+import matplotlib.pyplot as plt
 
 # Output path
 out_file = "lasertest.xdmf"
@@ -14,7 +15,7 @@ os.makedirs(output_dir, exist_ok=True)
 # Geometry
 gdim, fdim = 2, 1
 Nx, Ny = 100, 100
-length_half = 0.01
+length_half = 0.004
 domain = mesh.create_rectangle(
     MPI.COMM_WORLD,
     [np.array([-length_half, -length_half]), np.array([length_half, length_half])],  # 2cm x 2cm domain
@@ -26,21 +27,28 @@ Vt = fem.functionspace(domain, ("Lagrange", 1))
 # Dirichlet BCs on left, right, bottom (correct domain values!)
 def dirichlet_boundary(x):
     return (
-        np.isclose(x[0], -0.01) |
-        np.isclose(x[0],  0.01) |
-        np.isclose(x[1], -0.01)
+        np.isclose(x[0], -length_half) |
+        np.isclose(x[0],  length_half) |
+        np.isclose(x[1], -length_half) 
     )
 
 bc_facets = mesh.locate_entities_boundary(domain, fdim, dirichlet_boundary)
 bc_dofs = fem.locate_dofs_topological(Vt, fdim, bc_facets)
-bcs = [fem.dirichletbc(PETSc.ScalarType(0.0), bc_dofs, Vt)]
+T_room = 293.0  # Room temperature in Kelvin
+bcs = [fem.dirichletbc(PETSc.ScalarType(T_room), bc_dofs, Vt)]
 
 # Material + time params (steel-like)
-material_params = {"rho": 7850.0, "Cp": 500.0, "k_therm": 15.0}
+material_params = {"rho": 7850.0,    # density in kg/m³
+                   "Cp": 500.0,      # specific heat capacity in J/kg·K
+                   "k_therm": 15.0   # thermal conductivity in W/m·K
+                   }
+
 alpha = material_params["k_therm"] / (material_params["rho"] * material_params["Cp"])  # thermal diffusivity
 CFL_term  = (length_half /Nx)**2 / alpha
 print(f"CFL condition demands: dt<{CFL_term:.4f}")
-time_params = {"t_end": 5.0, "Nsteps": 3000}  
+
+time_params = {"t_end": 0.08,  # total time in seconds, the total time is given by the laser 
+                "Nsteps": 1000}  # number of time steps
 
 #instead of the implcicit solver, the CFL condition is checked here in order to ensure stability
 if time_params["t_end"] / time_params["Nsteps"] > CFL_term: 
@@ -48,14 +56,14 @@ if time_params["t_end"] / time_params["Nsteps"] > CFL_term:
     raise ValueError(f"Time step too large for stability: dt={time_params['t_end'] / time_params['Nsteps']:.4f} > CFL={CFL_term:.4f}")
   
 # Initial condition: zero everywhere
-def initial_condition(x): return np.full(x.shape[1], 0.0)
+def initial_condition(x): return np.full(x.shape[1], T_room)
 
 # Source term (none)
 source_term = fem.Constant(domain, PETSc.ScalarType(0.0))
 
-# Tag top boundary for Neumann BC (y = 0.01)
+# Tag top boundary for Neumann BC (y = 0.033) 
 from dolfinx.mesh import meshtags, locate_entities
-top_facets = locate_entities(domain, fdim, lambda x: np.abs(x[1] - 0.01) < 1e-8)
+top_facets = locate_entities(domain, fdim, lambda x: np.abs(x[1] - length_half) < 1e-8)
 values = np.full(len(top_facets), 1, dtype=np.int32)
 facet_tags = meshtags(domain, fdim, top_facets, values)
 
@@ -63,28 +71,37 @@ facet_tags = meshtags(domain, fdim, top_facets, values)
 # g = fem.Constant(domain, PETSc.ScalarType(1e5))  # W/m² # in case you want to test a cnstant heat flux
 # neumann_conditions = [(g, ds_top)]
 
+laser_params = {
+    "A": 0.15,           # absorptivity
+    "P": 70.0,         # power [W]
+    "R": 130e-6,        # beam radius [m]
+    "v": 0.1,          # scan speed [m/s]
+    "y0": length_half          # vertical position [m]
+}
+
 class MovingLaser:
-    def __init__(self, A=0.5, P=100.0, R=0.0015, v=0.01, y0=0.01):
-        self.peak = 2 * A * P / (np.pi * R**2)
-        self.R = R
-        self.v = v
-        self.y0 = y0
+    def __init__(self, params):
+        self.A = params["A"]
+        self.P = params["P"]
+        self.R = params["R"]
+        self.v = params["v"]
+        self.y0 = params["y0"]
         self.t = 0.0
+        self.peak = 2 * self.A * self.P / (np.pi * self.R**2)
 
     def __call__(self, x):
-        x0 = -0.01 + self.v * self.t
+        x0 = -length_half + self.v * self.t
         r2 = (x[0] - x0)**2 + (x[1] - self.y0)**2
         return self.peak * np.exp(-2 * r2 / self.R**2)
 
-laser = MovingLaser()
+
+laser = MovingLaser(laser_params)
 scanned_distance = laser.v * time_params["t_end"]  # total distance scanned by the laser
 
 if scanned_distance > 2*length_half:
     print(f"Warning: Scanned distance {scanned_distance:.4f} m exceeds domain size (0.02 m).")
 
 ds_top = ufl.Measure("ds", domain=domain, subdomain_data=facet_tags, subdomain_id=1)
-
-laser = MovingLaser()
 
 def g_laser(x, t):
     laser.t = t
@@ -107,3 +124,20 @@ time_series, center_temp, T_final = heatdiff_implicit_solver(
     output_filename=out_file,
     neumann_bcs=neumann_conditions
 )
+
+# ===================================================
+#              VALIDATION WITH GREEN'S FUNCTION
+# ===================================================
+
+import matplotlib.pyplot as plt
+
+
+plt.figure(figsize=(10, 6))
+plt.plot(time_series, center_temp, label="FEniCS solution for the top center point")
+plt.xlabel("Time (s)")
+plt.ylabel("Temperature (K)")
+plt.title("Temperature at the center of the top boundary over time")
+plt.grid()
+plt.legend()
+plt.savefig(os.path.join(script_dir, "temperature_center_top_boundary.png"))
+plt.show()  
